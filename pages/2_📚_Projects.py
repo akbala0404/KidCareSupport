8db410e39b46c54e46621246fd1f6abbbb2cecff
akbala0404 <81943684+akbala0404@users.noreply.github.com>
2023-07-29 19:29:07 +0600

    changes

---
+++ pages/2_📚_Projects.py
@@ -0,0 +1,41 @@
+import streamlit as st
+
+
+st.title("KidCare Privacy Policy")
+st.write("Effective Date: 28.07.2023")
+st.write("Welcome to KidCare!")
+
+st.write("KidCare (the 'App') is a parenting application that provides access to a range of tools and features to support parents in their parenting journey. I value your privacy and am dedicated to protecting your personal information.")
+st.write("This Privacy Policy outlines how I collect, use, disclose, and store your information when you use our App.")
+st.subheader("Your Consent")
+st.write("By accessing and using the App, you consent to the collection, use, disclosure, storage, and processing of your information as outlined in this Privacy Policy.")
+
+st.subheader("Updates to the Policy")
+st.write("KidCare may update or modify this Privacy Policy from time to time, with or without prior notice to users. Users are bound by the Privacy Policy in effect at the time of using the App.")
+
+st.subheader("Information We Collect")
+st.write("I highly prioritize your privacy and ensure that I only collect the minimum necessary information to provide our services effectively. The information I collect may include:")
+st.write("1. Account Information: When you create a KidCare account, I may collect your name, email address, and other relevant account details.")
+st.write("2. Usage Data: I may collect usage data related to your interaction with the App, such as the features you use, the content you access, and your preferences.")
+st.write("3. Device Information: I may collect information about your device, including the device type, operating system, and unique device identifiers.")
+st.write("4. Communications: If you reach out to my support team or communicate with me through email or other channels, I may collect and store those communications for quality assurance and support purposes.")
+
+st.subheader("Use of Third-Party Services")
+    
+st.write("KidCare utilizes third-party services, including Firebase by Google, for app analytics and functionality enhancement. Please note that these third-party services may collect certain information, but they do not have access to any of your personal details.")
+
+st.subheader("How I Use Your Information")
+st.write("KidCare uses your information for the following purposes:")
+st.write("1. Personalization: I use your information to personalize your experience within the App, providing content and features relevant to your preferences.")
+st.write("2. Communication: I may use your email address to send you important updates, notifications, and information related to the App.")
+st.write("3. Improvements: Your feedback and usage data help me enhance and improve the App's functionality and user experience.")
+st.write("4. Support: I may use your information to address your inquiries, provide technical support, and assist you with any issues related to the App.")
+
+st.subheader("Data Security and Confidentiality")
+st.write("KidCare takes data security seriously and employs industry-standard measures to protect your information from unauthorized access, disclosure, or alteration. I do not sell or share your personal information with third parties.")
+
+st.subheader("Unanswered Questions")
+st.write("If you have any unanswered questions or concerns regarding your privacy, kindly contact me at akbala20042020@mail.ru, and I will be delighted to assist you.")
+
+st.write("Thank you for choosing KidCare! I am committed to providing you with a safe, enjoyable, and rewarding parenting experience through my App.")
+
